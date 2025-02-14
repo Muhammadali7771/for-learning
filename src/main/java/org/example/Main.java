a53925f0431c99ea95f5ce39fb03275951c9812f
Muhammadali <akbarovmuhammadali777@gmail.com>
2025-02-14 14:41:25 +0500

    Commit from branch2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello everybody");
+        System.out.println("change from branch2");
     }
 }
